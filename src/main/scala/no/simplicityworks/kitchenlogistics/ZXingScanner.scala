2dfc228ddc097e531d96fa6c575983159bcaaf3d
thoraage <thoraageeldby@gmail.com>
2012-03-21 23:58:14 +0100

    Scanner reply function to stay permanent

--- src/main/scala/no/simplicityworks/kitchenlogistics/ZXingScanner.scala
+++ src/main/scala/no/simplicityworks/kitchenlogistics/ZXingScanner.scala
@@ -5,21 +5,20 @@ import android.content.{ActivityNotFoundException, Intent}
 
 object ZXingScanner {
   val scannerRequestCode = 6739477
+  var onScanSuccess: Option[(String) => Unit] = None
 }
 
 trait ZXingScanner extends Scanner with Dialogs {
   this: Activity with TypedActivity with Storage =>
 
-  var onScanSuccess: Option[(String) => Unit] = None
-
   override def startScanner(f: (String) => Unit) {
     try {
-      this.onScanSuccess = Some(f)
+      ZXingScanner.onScanSuccess = Some(f)
       val intent = new Intent("com.google.zxing.client.android.SCAN");
       startActivityForResult(intent, ZXingScanner.scannerRequestCode);
     } catch {
       case e: ActivityNotFoundException =>
-      this.onScanSuccess = None
+      ZXingScanner.onScanSuccess = None
       createInfoDialog(935723, R.string.scanNotInstalledTitle, R.string.scanNotInstalledMessage)
     }
   }
@@ -28,11 +27,11 @@ trait ZXingScanner extends Scanner with Dialogs {
     if (requestCode == ZXingScanner.scannerRequestCode) {
       if (resultCode == Activity.RESULT_OK) {
         val code = intent.getStringExtra("SCAN_RESULT")
-        onScanSuccess.map(_(code))
+        ZXingScanner.onScanSuccess.map(_(code))
       } else if (resultCode == Activity.RESULT_CANCELED) {
         createInfoDialog(8768343, R.string.noResultTitle, R.string.noResultMessage)
       }
-      onScanSuccess = None
+      ZXingScanner.onScanSuccess = None
     }
     // TODO super.onActivityResult(requestCode, resultCode, intent)
   }
