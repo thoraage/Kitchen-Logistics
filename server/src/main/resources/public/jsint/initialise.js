154c6c1ef32d80634a0c386994e1f7637f2606df
thoraage <thoraageeldby@gmail.com>
2013-01-26 22:33:39 +0100

    Added new bootstrap and angular

--- server/src/main/resources/public/jsint/initialise.js
+++ server/src/main/resources/public/jsint/initialise.js
@@ -1,3 +1,14 @@
+function PhoneListCtrl($scope) {
+  $scope.phones = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+}
+
 $(document).ready(function() {
     function isAppleDevice() {
         return (
@@ -13,7 +24,7 @@ $(document).ready(function() {
 
     $('#scanBtn').click(function() {
         var loc = window.location
-        var replyUrl = loc.protocol + '//' + loc.host + '/scan/products/{CODE}'
+        var replyUrl = loc.protocol + '//' + loc.host + '/scan/codes/{CODE}'
         if (isAppleDevice() || isAndroidDevice()) {
             window.location = (isAppleDevice() ? "zxing://scan/?ret=" : "http://zxing.appspot.com/scan?ret=") + replyUrl
         } else
