bd9613160e4c72a823810f30faf366f0416145ad
Thor Åge Eldby <tel@knowit.no>
2015-01-31 15:40:26 +0100

    Reformatted and removed unnecessary byte compare method

--- server/src/main/scala/no/simplicityworks/kitchenlogistics/RestPlanModule.scala
+++ server/src/main/scala/no/simplicityworks/kitchenlogistics/RestPlanModule.scala
@@ -17,16 +17,11 @@ import scala.slick.driver.H2Driver.simple._
 
 trait RestPlanModule extends PlanCollectionModule with DatabaseModule {
 
-  override def plans = authenticationPlan :: restPlan :: super.plans
+    override def plans = authenticationPlan :: restPlan :: super.plans
 
-  // TODO cache should empty itself
-  private val sessionCache = collection.mutable.Map[UUID, String]()
-  private val md5 = MessageDigest.getInstance("MD5")
-
-    def compare(bs1: Array[Byte], bs2: Array[Byte]) = {
-        if (bs1.size != bs2.size) false
-        else bs1.zip(bs2).forall(b => b._1 == b._2)
-    }
+    // TODO cache should empty itself
+    private val sessionCache = collection.mutable.Map[UUID, String]()
+    private val md5 = MessageDigest.getInstance("MD5")
 
     def getAuthenticatedUsername(uuid: String) = sessionCache.get(UUID.fromString(uuid))
 
@@ -56,16 +51,14 @@ trait RestPlanModule extends PlanCollectionModule with DatabaseModule {
             val user = database withSession { implicit session: Session =>
                 (for {user <- Query(Users) if user.username === name} yield user).firstOption
             }
-            if (user.exists(user => compare(user.password, md5.digest((Users.passwordSalt + pass).getBytes("UTF-8"))))) {
+            if (user.exists(user => user.password.sameElements(md5.digest((Users.passwordSalt + pass).getBytes("UTF-8"))))) {
                 val uuid = UUID.randomUUID
                 synchronized(sessionCache += (uuid -> name))
-                SetCookies(Cookie("auth", uuid.toString, maxAge = Some(1000*60*30))) ~> Ok
-            } else {
-                Unauthorized ~> ResponseString("Not authorized")
-            }
-        case req @ Path(Seg("rest" :: _)) =>
+                SetCookies(Cookie("auth", uuid.toString, maxAge = Some(1000 * 60 * 30))) ~> Ok
+            } else Unauthorized ~> ResponseString("Not authorized")
+        case req@Path(Seg("rest" :: _)) =>
             Unauthorized ~> ResponseString("Not authorized")
-  }
+    }
 
     private implicit val formats = Serialization.formats(NoTypeHints)
 
@@ -77,63 +70,67 @@ trait RestPlanModule extends PlanCollectionModule with DatabaseModule {
     private def extract: Params.Extract[Nothing, String] =
         new Params.Extract("code", Params.first ~> Params.nonempty)
 
-    private val restPlan = unfiltered.filter.Planify { Directive.Intent {
-      case Path(Seg("rest" :: "products" :: Nil)) =>
-        (for {
-          _ <- GET
-          _ <- Accepts.Json
-          Some(code) <- data.as.String named "code"
-          r <- request[Any]
-        } yield Ok ~> ResponseString(write(Products.findByCode(code)))
-          ).orElse(
-          for {
-            _ <- PUT
-            r <- request[Any]
-          } yield {
-            val id = Products.insert(read[Product](Body string r))
-            Ok ~> ResponseString(write(Map("id" -> id)))
-          })
-      case Path(Seg("rest" :: "items" :: Nil)) & AuthenticatedUser(user) =>
-        (for { _ <- GET; _ <- Accepts.Json; r <- request[Any] } yield {
-          val items = (database withSession { implicit session: Session =>
-            (for { item <- Items; product <- item.product } yield (product, item))
-              .groupBy(p => (p._1.id, p._1.name))
-              .map { case (id, pair) => (id, pair.length, pair.map(_._2.id).max) }.list
-          }).map(p => Map("count" -> p._2, "product" -> Map("id" -> p._1._1, "name" -> p._1._2), "lastItemId" -> p._3))
-          Ok ~> ResponseString(write(items))
-        }).orElse(
-          for { _ <- PUT; r <- request[Any] } yield {
-            val id = Items.insert(read[Item](Body string r).copy(userId = user.id))
-            Ok ~> ResponseString(write(Map("id" -> id)))
-          })
-      case Path(Seg("rest" :: "items" :: IntString(itemId) :: Nil)) =>
-        for {
-          _ <- DELETE
-        } yield {
-          Items.delete(itemId)
-          Ok ~> NoContent
-        }
+    private val restPlan = unfiltered.filter.Planify {
+        Directive.Intent {
+            case Path(Seg("rest" :: "products" :: Nil)) =>
+                (for {
+                    _ <- GET
+                    _ <- Accepts.Json
+                    Some(code) <- data.as.String named "code"
+                    r <- request[Any]
+                } yield Ok ~> ResponseString(write(Products.findByCode(code)))
+                    ).orElse(
+                        for {
+                            _ <- PUT
+                            r <- request[Any]
+                        } yield {
+                            val id = Products.insert(read[Product](Body string r))
+                            Ok ~> ResponseString(write(Map("id" -> id)))
+                        })
+            case Path(Seg("rest" :: "items" :: Nil)) & AuthenticatedUser(user) =>
+                (for {_ <- GET; _ <- Accepts.Json; r <- request[Any]} yield {
+                    val items = (database withSession { implicit session: Session =>
+                        (for {item <- Items if item.userId === user.id; product <- item.product} yield (product, item))
+                            .groupBy(p => (p._1.id, p._1.name))
+                            .map { case (id, pair) => (id, pair.length, pair.map(_._2.id).max)}.list
+                    }).map(p => Map("count" -> p._2, "product" -> Map("id" -> p._1._1, "name" -> p._1._2), "lastItemId" -> p._3))
+                    Ok ~> ResponseString(write(items))
+                }).orElse {
+                    for {_ <- PUT; r <- request[Any]} yield {
+                        val id = Items.insert(read[Item](Body string r).copy(userId = user.id))
+                        Ok ~> ResponseString(write(Map("id" -> id)))
+                    }
+                }
+            case Path(Seg("rest" :: "items" :: IntString(itemId) :: Nil)) =>
+                for {
+                    _ <- DELETE
+                } yield {
+                    Items.delete(itemId)
+                    Ok ~> NoContent
+                }
 
-      case Path(Seg("rest" :: "itemGroups" :: Nil)) =>
-        (for {
-          _ <- GET
-          _ <- Accepts.Json
-          r <- request[Any]
-        } yield Ok ~> ResponseString(write(ItemGroups.getAll))).orElse(
-          for {
-            _ <- PUT
-            r <- request[Any]
-          } yield {
-            val id = ItemGroups.insert(read[ItemGroup](Body string r))
-            Ok ~> ResponseString(write(Map("id" -> id)))
-          }
-        )
-    }
+            case Path(Seg("rest" :: "itemGroups" :: Nil)) =>
+                (for {
+                    _ <- GET
+                    _ <- Accepts.Json
+                    r <- request[Any]
+                } yield Ok ~> ResponseString(write(ItemGroups.getAll))).orElse(
+                        for {
+                            _ <- PUT
+                            r <- request[Any]
+                        } yield {
+                            val id = ItemGroups.insert(read[ItemGroup](Body string r))
+                            Ok ~> ResponseString(write(Map("id" -> id)))
+                        }
+                    )
+        }
     }
 
     object IntString {
-      def unapply(v: String) = try Some(v.toInt)
-      catch { case _: NumberFormatException => None }
+        def unapply(v: String) = try Some(v.toInt)
+        catch {
+            case _: NumberFormatException => None
+        }
     }
 
 }
