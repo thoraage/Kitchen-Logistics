d31fa8b279d8795cd090de9191d00d1c02355937
Thor Åge Eldby <tel@knowit.no>
2015-02-15 12:11:09 +0100

    Set port number from Heroku properties

--- src/main/scala/no/simplicityworks/kitchenlogistics/Application.scala
+++ src/main/scala/no/simplicityworks/kitchenlogistics/Application.scala
@@ -2,11 +2,13 @@ package no.simplicityworks.kitchenlogistics
 
 import org.eclipse.jetty.util.resource.Resource
 
+import scala.util.Properties
+
 trait Application extends App {
 
   val stack: RestPlanModule
 
-  val http = unfiltered.jetty.Http(5000)
+  val http = unfiltered.jetty.Http(Properties.envOrElse("PORT", "8080").toInt)
   http.current.setBaseResource(Resource.newResource(getClass.getResource("/public").getFile, false))
   stack.plans.foldLeft(http)((http, plan) => http.plan(plan)).run()
 
