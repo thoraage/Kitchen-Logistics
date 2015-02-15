294ee97ad4552c0c92cb366ebf4cdaff4b1a3b41
Thor Åge Eldby <tel@knowit.no>
2015-02-15 13:24:49 +0100

    Use external form of resource url to be able to read from jars

--- src/main/scala/no/simplicityworks/kitchenlogistics/Application.scala
+++ src/main/scala/no/simplicityworks/kitchenlogistics/Application.scala
@@ -9,7 +9,7 @@ trait Application extends App {
   val stack: RestPlanModule
 
   val http = unfiltered.jetty.Http(Properties.envOrElse("PORT", "8080").toInt)
-  http.current.setBaseResource(Resource.newResource(getClass.getResource("/public").getFile, false))
+  http.current.setBaseResource(Resource.newResource(getClass.getResource("/public").toExternalForm, false))
   stack.plans.foldLeft(http)((http, plan) => http.plan(plan)).run()
 
 }
