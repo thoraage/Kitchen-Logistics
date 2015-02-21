a98a06aeb3dd21ff7dab68328650cfcbdd38677f
Thor Åge Eldby <tel@knowit.no>
2015-02-21 14:06:37 +0100

    Created staging environment (local postgres)

--- src/test/scala/no/simplicityworks/kitchenlogistics/ApplicationDevelopment.scala
+++ src/test/scala/no/simplicityworks/kitchenlogistics/ApplicationDevelopment.scala
@@ -2,6 +2,17 @@ package no.simplicityworks.kitchenlogistics
 
 object ApplicationDevelopment extends Application {
 
-    lazy override val stack = new RestPlanModule with DatabaseModule with DevelopmentDatabaseProfileModule
+    lazy override val stack = new RestPlanModule with DatabaseModule {
+
+        override lazy val databaseProfile = new DatabaseProfile {
+            override val password = ""
+            override val username = "root"
+            override val driverClass = "org.h2.Driver"
+            override val jdbcUrl = "jdbc:h2:mem:test"
+            override val driver = scala.slick.driver.H2Driver
+            override val generation = DatabaseGeneration.slickDdl
+        }
+
+    }
 
 }
---
+++ src/test/scala/no/simplicityworks/kitchenlogistics/ApplicationStaging.scala
@@ -0,0 +1,18 @@
+package no.simplicityworks.kitchenlogistics
+
+object ApplicationStaging extends Application {
+
+    lazy override val stack = new RestPlanModule with DatabaseModule {
+
+        override lazy val databaseProfile = new DatabaseProfile {
+            override val driver = scala.slick.driver.PostgresDriver
+            override val generation = DatabaseGeneration.flyway
+            override val password: String = "kitlog"
+            override val username: String = "kitlog"
+            override val jdbcUrl: String = "jdbc:postgresql://localhost/kitlog"
+            override val driverClass: String = "org.postgresql.Driver"
+        }
+
+    }
+
+}
