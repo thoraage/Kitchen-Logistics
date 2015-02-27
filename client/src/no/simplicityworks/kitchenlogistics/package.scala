ae4c79261119e1f05fb45c41fe49fc18dc07eaec
Thor Åge Eldby <tel@knowit.no>
2015-02-27 22:50:55 +0100

    Moved files according to new android plugin

--- client/src/no/simplicityworks/kitchenlogistics/package.scala
+++ client/src/no/simplicityworks/kitchenlogistics/package.scala
@@ -3,9 +3,11 @@ package no.simplicityworks
 import android.view.View
 import android.view.View.OnClickListener
 
+import scala.language.implicitConversions
+
 package object kitchenlogistics {
 
-  implicit def toOnClickListener(f: (View) => Unit) = new OnClickListener {
+  implicit def toOnClickListener(f: (View) => Unit): OnClickListener = new OnClickListener {
     def onClick(v: View) {
       f(v)
     }
