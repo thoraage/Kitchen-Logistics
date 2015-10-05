9878eb11b15f8a411c45df064c9cd62a351008da
Thor Åge Eldby <tel@knowit.no>
2015-10-05 23:28:33 +0200

    Removed client library, seemingly we have overlapping bouncy castle implementations (maybe heroku and android plugins?)

--- project/Build.scala
+++ project/Build.scala
@@ -4,5 +4,6 @@ import Keys._
 object Build extends Build {
     lazy val clientApi = project.in(file("client-api"))
     lazy val server = project.in(file("server")).dependsOn(clientApi % "compile->test")
+    //lazy val client = project.in(file("client")).dependsOn(clientApi)
 }
 
