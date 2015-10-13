62b3bced0d4db9e71a6db3e5abc937406e9c0958
Thor Åge Eldby <tel@knowit.no>
2015-10-13 21:34:58 +0200

    Merged rest tests.

---
+++ server/src/test/scala/no/simplicityworks/kitchenlogistics/RestSpec.scala
@@ -0,0 +1,86 @@
+package no.simplicityworks.kitchenlogistics
+
+import java.util.Date
+
+import org.scalatest._
+
+import scala.concurrent.{Future, Await}
+import scala.concurrent.duration.Duration.Inf
+import scala.util.Random
+
+class RestSpec extends FeatureSpec with GivenWhenThen with KitLogSpecBase {
+
+    def await[T](future: Future[T]): T = Await.result(future, Inf)
+
+    val product =
+        await(client.storage.saveProduct(new client.Product(None, "fdjks", "ting", new Date)))
+    def createItemGroup =
+        await(client.storage.saveItemGroup(new client.ItemGroup(None, None, s"test${Random.nextInt()}", new Date)))
+    def createItem(itemGroup: client.ItemGroup) =
+        await(client.storage.saveItem(new client.Item(None, None, product.id.get, itemGroup.id.get, new Date)))
+    def allGroups = await(client.storage.findItemGroups())
+    def itemsOf(itemGroup: client.ItemGroup) =
+        await(client.storage.findItemsByGroup(Some(itemGroup)))
+
+    feature("Item group get all") {
+        scenario("Ok") {
+            assert(allGroups.seq.size === 1)
+        }
+    }
+
+    feature("Item group create") {
+        scenario("Ok") {
+            assert(allGroups.seq.size === 1)
+            val itemGroup = await(client.storage.saveItemGroup(new client.ItemGroup(None, None, "Mine", new Date)))
+            assert(itemGroup.id !== None)
+            assert(allGroups.seq.size === 2)
+        }
+    }
+
+    feature("Item group rename") {
+        scenario("Ok") {
+            val itemGroup = allGroups.head
+            await(client.storage.saveItemGroup(itemGroup.copy(name = "New name")))
+            assert(allGroups.filter(_.id == itemGroup.id).head.name === "New name")
+        }
+    }
+
+    feature("Item group delete") {
+        scenario("Ok") {
+            val itemGroup = await(client.storage.saveItemGroup(new client.ItemGroup(None, None, "Already dead", new Date)))
+            await(client.storage.removeItemGroup(itemGroup.id.get))
+            assert(allGroups.filter(_.id == itemGroup.id) === Nil)
+        }
+    }
+
+    feature("Item get") {
+        scenario("Ok") {
+            val itemGroup = createItemGroup
+            val otherItemGroup = createItemGroup
+            createItem(itemGroup)
+            createItem(itemGroup)
+            val result = itemsOf(itemGroup)
+            assert(result.size === 1 && result.head.count === 2)
+            assert(itemsOf(otherItemGroup).size === 0)
+        }
+    }
+
+    feature("Item create") {
+        scenario("Ok") {
+            val itemGroup = createItemGroup
+            createItem(itemGroup)
+            assert(1 === itemsOf(itemGroup).size)
+        }
+    }
+
+    feature("Item delete") {
+        scenario("Ok") {
+            val itemGroup = createItemGroup
+            val item = createItem(itemGroup)
+            client.storage.findItemsByGroup(Some(itemGroup))
+            await(client.storage.removeItem(item.id.get))
+            assert(0 === itemsOf(itemGroup).size)
+        }
+    }
+
+}
