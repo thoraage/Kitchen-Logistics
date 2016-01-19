e1afbf5c633de15468fe1824efeca40c0bb7bfaa
Thor Åge Eldby <tel@knowit.no>
2016-01-19 09:38:40 +0100

    Mention database in application trait in order to trigger database update

--- server/src/main/scala/no/simplicityworks/kitchenlogistics/Application.scala
+++ server/src/main/scala/no/simplicityworks/kitchenlogistics/Application.scala
@@ -6,10 +6,11 @@ import scala.util.Properties
 
 trait Application extends App {
 
-  val stack: RestPlanModule
+    val stack: RestPlanModule
 
-  val http = unfiltered.jetty.Http(Properties.envOrElse("PORT", "8080").toInt)
-  http.current.setBaseResource(Resource.newResource(getClass.getResource("/public").toExternalForm, false))
-  stack.plans.foldLeft(http)((http, plan) => http.plan(plan)).run()
+    val http = unfiltered.jetty.Http(Properties.envOrElse("PORT", "8080").toInt)
+    http.current.setBaseResource(Resource.newResource(getClass.getResource("/public").toExternalForm, false))
+    stack.database
+    stack.plans.foldLeft(http)((http, plan) => http.plan(plan)).run()
 
 }
