273330a5bd90b3fa25f2963738e2a8a4fb405ccd
Thor Åge Eldby <tel@knowit.no>
2016-04-02 13:53:44 +0200

    Removed unnecessary comments and code from auth module

--- client/src/main/scala/no/simplicityworks/kitchenlogistics/SelectingAuthenticationModule.scala
+++ client/src/main/scala/no/simplicityworks/kitchenlogistics/SelectingAuthenticationModule.scala
@@ -7,7 +7,7 @@ import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.{GoogleAuthUtil, UserRecoverableAuthException}
 import com.google.android.gms.common.api.GoogleApiClient.Builder
 import com.google.android.gms.plus.Plus
-import org.scaloid.common.WidgetHelpers
+import org.scaloid.common.{WidgetHelpers, _}
 
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
@@ -43,63 +43,40 @@ trait SelectingAuthenticationModule extends AuthenticationModule with GuiContext
         override def headers(wwwAuthenticate: Option[String]): Map[String, String] = {
             try {
                 Log.i(logContext, "Startet login")
-                // TODO: Trenger
                 val googleApiClient = new Builder(guiContext).addApi(Auth.CREDENTIALS_API).addApi(Plus.API).addScope(Plus.SCOPE_PLUS_LOGIN).build()
                 Log.i(logContext, "Prøver connect")
                 val result = googleApiClient.blockingConnect()
                 if (!result.isSuccess) {
                     Log.i(logContext, s"DOH, better try to get auth ${result.getErrorMessage}")
-//                    val promise = Promise[(Int, Intent)]()
                     val future = activityIntentBroker.getResponseOn(result.startResolutionForResult(guiContext, _))
-                        //                    activityResultPromises = activityResultPromises + (authenticationRequestCode -> promise)
-//                    result.startResolutionForResult(guiContext, authenticationRequestCode)
                     Await.result(future, Duration.Inf) match {
                         case (Activity.RESULT_OK, _) =>
-                            Log.i(logContext, "Got positive result")
+                            Log.i(logContext, "Got positive result on auth request")
                             headers(wwwAuthenticate)
-                        case (resultCode, _) => sys.error(s"Intent result code $resultCode")
+                        case (resultCode, _) =>
+                            sys.error(s"Intent result code $resultCode")
                     }
                 } else {
-
-                    Log.i(logContext, s"#### Resolution is: $result")
-
-                    //                        val manager = AccountManager.get(MainActivity.this)
-                    //                        val list = manager.getAccounts
-
-                    //                        Log.i(getClass.getSimpleName, s"#### Fant antall: ${list.size}")
-                    //                        for (account <- list) {
-                    //                            if (account.`type`.equalsIgnoreCase("com.google")) {
-                    //                                Log.i(getClass.getSimpleName, s"#### Fant: ${account.name}")
-                    //                            }
-                    //                        }
-
-                    //list.map(_.name).headOption.orNull
                     val accountName = Plus.AccountApi.getAccountName(googleApiClient)
                     Log.i(logContext, s"Account name = $accountName")
                     if (accountName == null) sys.error("Not logged into google account")
                     val account = new Account(accountName, GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE)
                     val scopes = "oauth2:https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email"
-                    // "audience:server:client_id:" + clientId
                     val token = GoogleAuthUtil.getToken(guiContext, account, scopes)
-                    Log.i(logContext, s"##### Token: $token")
-                    WidgetHelpers.toast(s"Token is $token")
+                    WidgetHelpers.toast(R.string.autenticated.r2String)
                     val encodedString = Base64.encodeToString(token.getBytes, Base64.NO_WRAP)
-                    Log.i(logContext, s"##### Encoded token: $encodedString")
                     Map("Authorization" -> s"GoogleToken $encodedString")
                 }
             } catch {
                 case e: UserRecoverableAuthException =>
-                    Log.i(logContext, s"${e.getMessage}")
+                    Log.i(logContext, s"User recoverable auth exception: ${e.getMessage}")
                     val future = activityIntentBroker.getResponseOn(guiContext.startActivityForResult(e.getIntent, _))
-//                        startActivityForResult(e.getIntent, authenticationRequestCode)
-//                    val promise = Promise[(Int, Intent)]()
                     Await.result(future, Duration.Inf) match {
                         case (Activity.RESULT_OK, _) =>
-                            Log.i(logContext, "Got positive result")
+                            Log.i(logContext, "Got positive result on auth resolve")
                             headers(wwwAuthenticate)
                         case (resultCode, _) => sys.error(s"Intent result code $resultCode")
                     }
-                //GooglePlayServicesAvailabilityException. This is a specific type of UserRecoverableAuthException indicating that the user's current version of Google Play services is outdated. Although the recommendation above for UserRecoverableAuthException also works for this exception, calling startActivityForResult() will immediately send users to Google Play Store to install an update, which may be confusing. So you should instead call getConnectionStatusCode() and pass the result to GooglePlayServicesUtil.getErrorDialog(). This returns a Dialog that includes an appropriate message and a button to take users to Google Play Store so they can install an update.
             }
 
         }
