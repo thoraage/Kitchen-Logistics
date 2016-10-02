2c2fc5880687db0409b7203d48a1055cfe6309b9
Thor Åge Eldby <tel@knowit.no>
2016-10-02 19:03:22 +0200

    Changed date type to timestamp

---
+++ server/src/main/resources/db/migration/V006__Change_date_to_timestamp.sql
@@ -0,0 +1,4 @@
+alter table "global_product" alter "created" type timestamp;
+alter table "user" alter "created" type timestamp;
+alter table "user_item_group" alter "created" type timestamp;
+alter table "user_item" alter "created" type timestamp;
